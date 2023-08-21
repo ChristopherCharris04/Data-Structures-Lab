332716d61d51de886c940361bceb3070c4e3fedb
ChristopherCharris04 <220071601050@crescent.education>
2023-08-21 21:22:21 +0530

    Demo Commit

---
+++ Prog7.py
@@ -0,0 +1,10 @@
+nterms=int(input("How many terms?"))
+n1,n2=0,1
+count=0
+if nterms<=0:
+    print("Enter positive number")
+elif nterms==1:
+    print("Fibonacci Sequence upto",nterms,"=")
+    print(n1)
+else:
+    print("Fibonacci series")
