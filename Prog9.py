b04aad1e3ce78a991d7c7c05af3cc6564f9f68a4
ChristopherCharris04 <220071601050@crescent.education>
2023-08-22 22:24:04 +0530

    Done

---
+++ Prog9.py
@@ -0,0 +1,7 @@
+x=int(input('Enter first value = '))
+y=int(input('Enter second value = '))
+z=x
+x=y
+y=z
+print('The first value afterswapping = {}'.format(x))
+print('The second value afterswapping = {}'.format(y))
