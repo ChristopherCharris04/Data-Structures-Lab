9a0c6d471e340459dd63e8691a15bb4ef7a3eee6
ChristopherCharris04 <220071601050@crescent.education>
2023-08-22 20:40:32 +0530

    Commit

--- Prog7.py
+++ Prog7.py
@@ -8,3 +8,9 @@ elif nterms==1:
     print(n1)
 else:
     print("Fibonacci series")
+    while count < nterms:
+        print(n1) 
+        nth=n1+n2
+        n1=n2
+        n2=nth
+        count += 1
